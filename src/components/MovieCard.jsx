759db708a0dc8ed89034c479c25bbe9853daa9e1
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-01-02 11:35:21 +0300

    Refactor MovieCard component and update CSS styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,27 +18,29 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
     }
   };
   return (
-    <div
-      className="movie"
-      id="container"
-      onClick={() => navigate("/details/" + id)}
-    >
-      <img
-        loading="lazy"
-        src={poster_path ? IMG_API + poster_path : defaultImage}
-        alt="movie-card"
-      />
-      <div className="flex align-baseline justify-between p-1 text-white">
-        <h5>{title}</h5>
-        {currentUser && (
-          <span className={`tag ${getVoteClass(vote_average)}`}>
-            {vote_average.toFixed(1)}
-          </span>
-        )}
-      </div>
-      <div className="movie-over">
-        <h2>Overview</h2>
-        <p>{overview}</p>
+    <div className="movie-wrapper">
+      <div
+        className="movie"
+        id="container"
+        onClick={() => navigate("/details/" + id)}
+      >
+        <img
+          loading="lazy"
+          src={poster_path ? IMG_API + poster_path : defaultImage}
+          alt="movie-card"
+        />
+        <div className="flex align-baseline justify-between p-1 text-white">
+          <h5>{title}</h5>
+          {currentUser && (
+            <span className={`tag ${getVoteClass(vote_average)}`}>
+              {vote_average.toFixed(1)}
+            </span>
+          )}
+        </div>
+        <div className="movie-over">
+          <h2>Overview</h2>
+          <p>{overview}</p>
+        </div>
       </div>
     </div>
   );
